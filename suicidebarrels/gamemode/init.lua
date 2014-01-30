eb26310ec24e55e6a1b02a8f6fbd5520bdc75922
shomi32007 <shomi32007@yahoo.com>
2014-01-30 01:20:29 -0500

    Update init.lua

--- suicidebarrels/gamemode/init.lua
+++ suicidebarrels/gamemode/init.lua
@@ -5,7 +5,16 @@ include( "shared.lua" )
 include("player.lua")
 
 function GM:PlayerInitialSpawn( ply )
- 
     ply:SetTeam( 3 )
- 
 end 
+
+function team_1( ply )
+    ply:SetTeam( 1 )
+end
+ 
+function team_2( ply )
+    ply:SetTeam( 2 )
+end
+ 
+concommand.Add( "team_1", team_1 )
+concommand.Add( "team_2", team_2 )
