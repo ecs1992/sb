40661d6f42b07e1836b8df6283230b440b592533
shomi32007 <shomi32007@yahoo.com>
2014-01-30 00:21:10 -0500

    Update shared.lua
    Added Teams -- team.SetUp

--- suicidebarrels/gamemode/shared.lua
+++ suicidebarrels/gamemode/shared.lua
@@ -3,6 +3,10 @@ GM.Author = "CAYENNE WEST"
 GM.Email = "NO@YEAH.NO"
 GM.Website = "WorldStarHipHop.com"
 
+team.SetUp( 1, "Barrel", Color( 255, 0, 0, 255 ) )
+team.SetUp( 2, "Hunter", Color( 0, 0, 255, 255 ) )
+team.SetUp( 3, "Guest", Color( 0, 255, 0, 255 ) )
+
 function GM:Initialize()
 
 end 
