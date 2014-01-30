004ea25025fc499ae1252d193ca12f64ba6f0497
picklebutt <ecs1992@gmail.com>
2014-01-30 01:01:58 -0500

    Update player_barrel.lua

--- suicidebarrels/gamemode/player_class/player_barrel.lua
+++ suicidebarrels/gamemode/player_class/player_barrel.lua
@@ -1,3 +1,5 @@
+AddCSLuaFile()
+
 local PLAYER = {} 
 
 // Some settings for the class
@@ -5,11 +7,19 @@ PLAYER.DisplayName			= "Barrel"
 PLAYER.WalkSpeed 			= 230
 PLAYER.CrouchedWalkSpeed 	= 0
 PLAYER.RunSpeed				= 230
-PLAYERDuckSpeed				= 0
+PLAYER.DuckSpeed				= 0
 PLAYER.DrawTeamRing			= false
 
 function PLAYER:Loadout()
-
+  self.Player:RemoveAllAmmo()
+  self.Player:Give( "weapon_shotgun" )
 end
+
+function PLAYER:SetModel()
+    local mdl = "models/props_c17/oildrum001_explosive.mdl"
+    util.PrecacheModel(mdl)
+    self.Player:SetModel(mdl)
+    self.Player:SetColor(Color(255,255,255,255)
+  end
 
 player_manager.RegisterClass( "Barrel", PLAYER, nil )
